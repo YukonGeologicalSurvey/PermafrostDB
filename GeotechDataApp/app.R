ca11ed86150d89297f7712e33f3227d4cc2fd0e3
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2020-03-04 16:34:25 -0800

    Fixed mymap height

--- GeotechDataApp/app.R
+++ GeotechDataApp/app.R
@@ -132,6 +132,7 @@ ui <- function(request){fluidPage(
         .navbar-default .navbar-nav > .active > a:focus,
         .navbar-default .navbar-nav > .active > a:hover {color: white;background-color: #d99700;}
         .navbar-default .navbar-nav > li > a:hover {color: white;background-color:#d99700;}
+        #mymap {height: calc(100vh - 250px) !important; 
                   ")),
   
   navbarPage(title = "", id = "Navbar", selected = "Map",
@@ -139,7 +140,7 @@ ui <- function(request){fluidPage(
              tabPanel(title=HTML("</a></li><li><a href='http://emr-permafrost:3838/Yukon-Permafrost-Homepage/#' target='_self' >Home")),
              
              tabPanel("Map", 
-                      leafletOutput("mymap", height='750') %>% 
+                      leafletOutput("mymap") %>% 
                         withSpinner(color="#0097A9"),
                       br(),
                       downloadButton("downloadLoc.csv", "Download locations CSV"),
